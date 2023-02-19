29f85202a5801ae475f32ac4c71459adea2dbc1f
Krupickas <114585256+Krupickas@users.noreply.github.com>
2023-02-19 19:21:43 +0100

    Update cs.lua

--- krp_gym/locales/cs.lua
+++ krp_gym/locales/cs.lua
@@ -1,26 +1,30 @@
-   Locales['cs'] = {
-
-      --eye
-      ['bench_exercise'] = 'Cvičení bench',
-      ['bike_exercise'] = 'Cvičení na kole',
-      ['flex'] = 'Flexit',
-      ['chins_exercise'] = 'cvičení pushup',
-      ['free_weight_exercise'] = 'cvičení s volnou váhou',
-      ['one_hand_exercise'] = 'Cvičení s jednou rukou',
-      ['situps_exercise'] = 'sedy-lehy',
-
-
-
-      --NOTIFY
-      ['you_had_good_exercise'] = 'Měl jsi dobré cvičení',
-      ['you_had_bad_exericise'] = 'Měl jsi špatné cvičení',
-      ['catching_breath'] = 'lapaš po dechu',
-
-      --spinning busy
-      ['flexing_muscle'] = 'Flexiš',
-
-      -- vstupní dialog
-      ['how_long_to_flex'] = 'Jak dlouho flexit ?',
-      ['description'] = 'V milisekundách, takže - 5 sec je 5000',
-
-         }
+local Translations = {
+  eye = {
+    bench_exercise = 'Cvičení na lavičce',
+    bike_exercise = 'Cvičení na kole',
+    flex = 'Flex Muscle',
+    chins_exercise = 'Cvičení na bradlech',
+    free_weight_exercise = 'Cvičení s volnou váhou',
+    one_hand_exercise = 'Cvičení s jednou rukou',
+    situps_exercise = 'Cvičení vsedě',
+  },
+    success = {
+      you_had_good_exericise = 'Měl jsi dobré cvičení',
+  },
+     error = {
+    you_had_bad_exericise = 'Špatně jste cvičil',
+    catching_breath = 'Lapal jsem po dechu',
+  },
+     input = {
+    flexing_muscle = 'Ohebný sval',
+  
+    -- vstupní dialog
+    how_long_to_flex = 'Jak dlouho se má sval prohýbat ?',
+    description = 'V milisekundách, takže - 5 s je 5000',
+  
+}
+}
+Lang = Lang or Locale:new({
+  phrases = Translations,
+  warnOnMissing = true
+})
