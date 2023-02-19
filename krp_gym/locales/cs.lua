85d08c64cc0c88d42e402f614554077332c4942a
Krupickas <114585256+Krupickas@users.noreply.github.com>
2023-02-19 14:23:22 +0100

    Update cs.lua

--- krp_gym/locales/cs.lua
+++ krp_gym/locales/cs.lua
@@ -1,19 +1,26 @@
-Locales['cs'] = {
+   Locales['cs'] = {
 
-   --eye
-   ['bench_exercise'] = 'Bench exercise',
-   ['bike_exercise'] = 'Cvičenie na bicykli',
-   ['flex'] = 'Svalová flexia',
-   ['chins_exercise'] = 'Cvičenie na bradu',
-   ['free_weight_exercise'] = 'Cvičenie s voľnou váhou',
-   ['one_hand_exercise'] = 'Cvičenie s jednou rukou',
-   ['situps_exercise'] = 'Cvičenie sed-ľah',
- 
- 
- 
-   --NOTIFY
-   ['you_had_good_exericise'] = 'Mal si dobré cvičenie',
-   ['you_had_bad_exericise'] = 'Mal si zlé cvičenie',
-   ['catching_breath'] = 'lapal po dychu',
+      --eye
+      ['bench_exercise'] = 'Cvičení bench',
+      ['bike_exercise'] = 'Cvičení na kole',
+      ['flex'] = 'Flexit',
+      ['chins_exercise'] = 'cvičení pushup',
+      ['free_weight_exercise'] = 'cvičení s volnou váhou',
+      ['one_hand_exercise'] = 'Cvičení s jednou rukou',
+      ['situps_exercise'] = 'sedy-lehy',
 
-      }
+
+
+      --NOTIFY
+      ['you_had_good_exercise'] = 'Měl jsi dobré cvičení',
+      ['you_had_bad_exericise'] = 'Měl jsi špatné cvičení',
+      ['catching_breath'] = 'lapaš po dechu',
+
+      --spinning busy
+      ['flexing_muscle'] = 'Flexiš',
+
+      -- vstupní dialog
+      ['how_long_to_flex'] = 'Jak dlouho flexit ?',
+      ['description'] = 'V milisekundách, takže - 5 sec je 5000',
+
+         }
