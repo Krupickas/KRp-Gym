bb383db15a2660f54b38e77579579fc7ed865373
Krupickas <114585256+Krupickas@users.noreply.github.com>
2023-02-19 19:21:54 +0100

    Update en.lua

--- krp_gym/locales/en.lua
+++ krp_gym/locales/en.lua
@@ -1,26 +1,32 @@
-Locales['en'] = {
-   --eye
-  ['bench_exercise'] = 'Bench exercise',
-  ['bike_exercise'] = 'Bike exercise',
-  ['flex'] = 'Flex Muscle',
-  ['chins_exercise'] = 'Chins exercise',
-  ['free_weight_exercise'] = 'Free Weight exercise',
-  ['one_hand_exercise'] = 'One hand exercise',
-  ['situps_exercise'] = 'Situps exercise',
-
-
-
-  --NOTIFY
-  ['you_had_good_exericise'] = 'You had good exercise',
-  ['you_had_bad_exericise'] = 'You had bad exercise',
-  ['catching_breath'] = 'Gasping for breath',
+local Translations = {
+   eye = {
+  bench_exercise = 'Bench exercise',
+  bike_exercise = 'Bike exercise',
+  flex = 'Flex Muscle',
+  chins_exercise = 'Chins exercise',
+  free_weight_exercise = 'Free Weight exercise',
+  one_hand_exercise = 'One hand exercise',
+  situps_exercise = 'Situps exercise',
+},
+  success = {
+    you_had_good_exericise = 'You had good exercise',
+},
+   error = {
+  you_had_bad_exericise = 'You bad exercise',
+  catching_breath = 'Gasping for breath',
+},
 
 
   -- busy spinner
-
-  ['flexing_muscle'] = 'Flexing muscle',
+ input = {
+  flexing_muscle = 'Flexing muscle',
 
   -- input dialog
-  ['how_long_to_flex'] = 'How long to flex ?',
-  ['description'] = 'In miliseconds so - 5 sec is 5000',
+  how_long_to_flex = 'How long to flex ?',
+  description = 'In miliseconds so - 5 sec is 5000',
 }
+}
+Lang = Lang or Locale:new({
+  phrases = Translations,
+  warnOnMissing = true
+})
