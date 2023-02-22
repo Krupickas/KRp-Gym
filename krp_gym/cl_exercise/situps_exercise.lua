d1dce3f3ea05100e236c4d2974bc2f16fa829ad4
Krupickas <114585256+Krupickas@users.noreply.github.com>
2023-02-22 17:44:59 +0100

    Update situps_exercise.lua

--- krp_gym/cl_exercise/situps_exercise.lua
+++ krp_gym/cl_exercise/situps_exercise.lua
@@ -68,7 +68,7 @@ exports.qtarget:AddCircleZone("situps2", vec3(-1203.4559, -1567.7660, 4.0093), 0
                 TaskStartScenarioInPlace(playerPed, "world_human_sit_ups", 0, true)
 Wait(5000)
 local situps = lib.skillCheck({'easy', 'easy', 'easy', 'easy', 'easy', {areaSize = 60, speedMultiplier = 1}, 'medium'})
-if situps then
+if situpsa then
     lib.notify({
         title = 'Gym',
         description = _U('you_had_good_exericise'),
@@ -76,19 +76,14 @@ if situps then
     })
     ClearPedTasks(PlayerPedId())
     Wait(2000)
-    FreezeEntityPosition(playerPed, false)
     if Config.SkillSystem == 'gamz' then
         exports["gamz-skillsystem"]:UpdateSkill("Stamina", 0.5)
+        exports["gamz-skillsystem"]:UpdateSkill("Strength", 0.5)
        elseif Config.SkillSystem == 'b1skillz' then
         exports["B1-skillz"]:UpdateSkill("Stamina", 0.5)
+        exports["B1-skillz"]:UpdateSkill("Strength", 0.5)
        end
 else
-    lib.notify({
-        title = 'Gym',
-        description = _U('you_had_bad_exericise'),
-        type = 'error'
-    })
-    FreezeEntityPosition(playerPed, false)
     ClearPedTasks(PlayerPedId())
     Wait(4500)
     lib.progressBar({
